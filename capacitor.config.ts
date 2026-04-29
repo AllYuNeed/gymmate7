f49b3ec14350bba64041f361da93504067958bf7
AllYuNeed <allyuneed37@gmail.com>
2026-04-29 06:23:51 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.d1e35de5b480453d9b61e9ba38837ebe',
-  appName: 'mortalgyms',
+  appId: 'com.mortalgyms.app',
+  appName: 'Mortal Gyms',
   webDir: 'dist',
   server: {
-    url: 'https://d1e35de5-b480-453d-9b61-e9ba38837ebe.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
+    url: 'https://mortalgyms.com',
+    cleartext: false,
   },
 };
 
